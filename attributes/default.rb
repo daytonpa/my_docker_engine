e1944b4e70b476d6cfa381d13bf3304d194819a8
Patrick Hugh Dayton <daytonpa@gmail.com>
2017-06-15 15:53:37 -0400

    Updated attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -37,25 +37,11 @@ default['my_docker_engine'].tap do |dock|
   dock['components'] = ['main', 'stable']
   dock['distribution'] = "#{node['platform']}-#{node['lsb']['codename']}"
 
-  # Choose your desired Docker installation method.  You may pick 'package'
-  # or 'repo'.
-  # Default is set to 'package'
-  dock['installation_method'] = 'package'
-
-  # case node['platform_family']
-  # when 'debian'
-  #   dock['url'] = 'https://apt.dockerproject.org/repo'
-  #   dock['key'] = 'https://apt.dockerproject.org/gpg'
-  # when 'rhel'
-  #   plat_v = node['platform_version'].split('.').first
-  #   dock['url'] = "https://yum.dockerproject.org/repo/main/centos/#{plat_v}/"
-  #   dock['key'] = 'https://yum.dockerproject/gpg'
-  # end
-
   # Default Docker image
-  dock['docker_images'] = ['hello-world']
+  dock['dockerfiles'] = %w(cowsay nodejs_server)
 
   # Configuration options
+  dock['ip_address'] = '127.0.0.1'
   dock['config'] = nil
 
 end
