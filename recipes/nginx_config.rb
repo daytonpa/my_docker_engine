8f900fd42024a220629ffe9fe88aaf0b718768c0
Patrick Hugh Dayton <daytonpa@gmail.com>
2017-06-15 15:58:31 -0400

    Create nginx_config.rb

---
+++ recipes/nginx_config.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook:: my_docker_engine
+# Recipe:: nginx_config
+# Author:: Patrick Dayton
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+package 'nginx'
+
+service 'nginx' do
+  supports [:start, :stop, :restart, :reload]
+  action [:start, :enable]
+end
+
+cookbook_file '/etc/nginx/nginx.conf' do
+  # owner ['my_docker_engine']['user']
+  # group ['my_docker_engine']['group']
+  owner 'vagrant'
+  group 'vagrant'
+  mode '0644'
+  source 'nginx.conf'
+  notifies :restart, 'service[nginx]', :immediately
+end
